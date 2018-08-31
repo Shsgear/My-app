dbf371653d25cf41332426a84acbfccc781a3e88
孙皓 <shsgear@qq.com>
2018-08-31 23:58:46 +0800

    fix npm scripts

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import Hello from './component/Hello/Hello';
-import './index.css';
+// import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
